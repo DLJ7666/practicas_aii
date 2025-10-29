3cddce5596276ce77df9dd297cd7eda50e30146f
DLJ7666 <jarguezduran@gmail.com>
2025-10-29 12:28:35 +0100

    trabajo clase django 29-10

---
+++ django/intro_django/principal/views.py
@@ -0,0 +1,6 @@
+from principal.models import Bebida
+from django.shortcuts import render
+
+def lista_bebidas(request):
+    bebidas = Bebida.objects.all()
+    return render(request, 'lista_bebidas.html', {'lista': bebidas})
